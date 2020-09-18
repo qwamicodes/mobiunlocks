f4b74101486f57baf1a940930f46bb70df20ddea
qwami-vibes <qwaedjekum@gmail.com>
2020-09-18 07:14:52 +0000

    Finish with the featuressection

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import { elements } from  './views/base';
 
 //Onload page function that starts the svg magic 
 document.onload = UICtrl.headerSVG();
+document.onload = controller.checkNav();
 
 //Onscroll function to trigger the background
 document.addEventListener('scroll', controller.checkNav);
