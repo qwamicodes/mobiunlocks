9252fcbb13dc5919b01c28e22c37d8f8913c706a
qwami-vibes <qwaedjekum@gmail.com>
2020-10-08 02:41:06 +0000

    adding functionalities to the payment

--- src/js/app.js
+++ src/js/app.js
@@ -31,10 +31,27 @@ document.querySelector(elements.cancelPayment).addEventListener('click', () => {
         const carNetwork = form['carrier-network'];
         const carImei = form['carrier-imei'];
         const btn = form.elements[`${form.elements.length - 1}`];
+
+        if(form.elements[0].checked) {
+            document.querySelectorAll(elements.ipadCarrier).forEach(el => {
+                el.style.display = 'none'; 
+            });
+            document.querySelectorAll(elements.iphoneCarrier).forEach(el => {
+                el.style.display = 'block'; 
+            });
+        } else if(form.elements[1].checked) {
+            document.querySelectorAll(elements.iphoneCarrier).forEach(el => {
+                el.style.display = 'none'; 
+            });
+            document.querySelectorAll(elements.ipadCarrier).forEach(el => {
+                el.style.display = 'block'; 
+            });
+        };
         
-        console.log(checkPhone.value)
         if(checkPhone.value === 'on' && carImei.value.length >= 15 && form.checkValidity()) {
             controller.enableField(btn);
+        } else {
+            controller.disableField(btn);
         };
     });
 });
@@ -50,6 +67,8 @@ document.querySelector(elements.cancelPayment).addEventListener('click', () => {
         
         if(carImei.value.length >= 15 && form.checkValidity()) {
             controller.enableField(btn);
+        } else {
+            controller.disableField(btn);
         };
     });
 });
@@ -63,9 +82,27 @@ document.querySelector(elements.cancelPayment).addEventListener('click', () => {
         const phoneModel = form['unlocking-model'];
         const carImei = form['unlocking-imei'];
         const btn = form.elements[`${form.elements.length - 1}`];
+
+        if(form.elements[0].checked) {
+            document.querySelectorAll(elements.ipadUnlock).forEach(el => {
+                el.style.display = 'none';
+            });
+            document.querySelectorAll(elements.iphoneUnlock).forEach(el => {
+                el.style.display = 'block'; 
+            });
+        } else if(form.elements[1].checked) {
+            document.querySelectorAll(elements.iphoneUnlock).forEach(el => {
+                el.style.display = 'none'; 
+            });
+            document.querySelectorAll(elements.ipadUnlock).forEach(el => {
+                el.style.display = 'block';
+            });
+        };
         
         if(checkPhone.value === 'on' && carImei.value.length >= 15 && form.checkValidity()) {
             controller.enableField(btn);
+        } else {
+            controller.disableField(btn);
         };
     });
 });
@@ -75,6 +112,34 @@ document.querySelector(elements.cancelPayment).addEventListener('click', () => {
         e.preventDefault();
         //TODO:
         //grab the values form the form
+        switch (e.target.id) {
+            case 'form-carrier':
+                const phoneModel = e.target['carrier-model'].value;
+                const modelName = e.target['carrier-model'][e.target['carrier-model'].selectedIndex].innerText;
+                const carNetwork = e.target['carrier-network'].value;
+                const carImei = e.target['carrier-imei'].value;
+
+                new controller.Pay('carrier', phoneModel, modelName, carNetwork, carImei).calcPrice().insertPay();
+                
+                break;
+            case 'form-imei':
+                const imeiNetwork = e.target['imei-network'].value;
+                const imeiImei = e.target['imei-imei'].value;
+
+                new controller.Pay('imei', null, null, imeiNetwork, imeiImei).calcPrice().insertPay();
+                
+                break;
+            case 'form-unlocking':
+                const unlockingModel = e.target['unlocking-model'].value;
+                const modName = e.target['unlocking-model'][e.target['unlocking-model'].selectedIndex].innerText;
+                const unlockingImei = e.target['unlocking-imei'].value;
+
+                new controller.Pay('unlocking', unlockingModel, modName, null, unlockingImei).calcPrice().insertPay();
+                
+            break;
+        }
+
         controller.showPay();
+        e.target.reset();
     });
 });
--- src/js/models/controller.js
+++ src/js/models/controller.js
@@ -83,15 +83,90 @@ export const hidePay = () => {
 };
 
 export class Pay {
-    constructor(type, model, network, imei) {
+    constructor(type, model, modelName, network, imei) {
         this.type = type,
-        this.model = model,
+        this.model = parseInt(model, 10),
+        this.modelName = modelName,
         this.network = network,
         this.imei = imei
+        this.price = (0).toFixed(2);
     }
 
     calcPrice () {
+        switch (this.type) {
+            case 'carrier':
+                if(this.network === 'at&t' || this.network === 'verizon') {
+                    if(this.model >= 101 && this.model <= 109 ) {
+                        this.price =  (170).toFixed(2);
+                    } else if(this.model >= 110 && this.model <= 113) {
+                        this.price =  (270).toFixed(2);
+                    } else if(this.model >= 114 && this.model <= 118) {
+                        this.price =  (370).toFixed(2);
+                    } else if(this.model >= 119 && this.model <= 122) {
+                        this.price =  (470).toFixed(2);
+                    };
+                } else if(this.network === 'sprint') {
+                } else if(this.network === 'tmobile') {
+                } else if(this.network === '02' || this.network === 'tesco') {
+                };
 
+                break;
+            case 'imei':
+                this.price = (25).toFixed(2);
+                break;
+            case 'unlocking':
+                break;
+        };
         return this;
+    }
+
+    insertPay ()  {
+        if(this.type === 'carrier') {
+            let html = `
+                <div class="payment__items--item">
+                <span class="payment__items--main">type</span>
+                <span class="payment__items--sub">${this.type}</span>
+                </div>
+                <div class="payment__items--item">
+                <span class="payment__items--main">model</span>
+                <span class="payment__items--sub">${this.modelName}</span>
+                </div>
+                <div class="payment__items--item">
+                <span class="payment__items--main">carrier</span>
+                <span class="payment__items--sub">${this.network}</span>
+                </div>
+                <div class="payment__items--item">
+                <span class="payment__items--main">IMEI</span>
+                <span class="payment__items--sub">${this.imei}</span>
+                </div>
+            `;
+            document.querySelector(elements.paymentItems).innerHTML = html;
+        } else if(this.type === 'imei') {
+            let html = `
+                <div class="payment__items--item">
+                    <span class="payment__items--main">type</span>
+                    <span class="payment__items--sub">${this.type}</span>
+                </div>
+                <div class="payment__items--item">
+                    <span class="payment__items--main">carrier</span>
+                    <span class="payment__items--sub">${this.network}</span>
+                </div>
+                <div class="payment__items--item">
+                    <span class="payment__items--main">IMEI</span>
+                    <span class="payment__items--sub">${this.imei}</span>
+                </div>
+            `;
+            document.querySelector(elements.paymentItems).innerHTML = html;
+        } else if(this.type === 'unlocking') {
+
+        } 
+   
+        document.querySelectorAll(elements.paymentPrice).forEach(el => {
+            el.innerHTML = `GHC ${this.price}`;
+        });
+
+        const price = this.price.split('.');
+        document.querySelector('.payment-price-big').innerHTML = `${price[0]}`;
+        document.querySelector('.payment-price-small').innerHTML = `.${price[1]} GHC`;
     }
 }
