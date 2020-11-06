edaf3c0cffa0944a02361188e743ceb66e4c9b24
Machele-codez <smithbeblack@gmail.com>
2020-11-06 12:20:56 +0000

    documented dashboard population in code

--- src/js/api.js
+++ src/js/api.js
@@ -183,6 +183,7 @@ export const searchTaskByTracking = trackingID => {
                 // if task was found and has been completed without error
                 if (taskDetail['completed'] && !taskDetail['error']) {
                     alert(taskDetail['results']);
+                    // ! check task type and display results accordingly
                     // switch (taskDetail['task_type']) {
                     //     case "imei":
                     //         alert(taskDetail['results']);
@@ -212,32 +213,51 @@ export const getAllTasks = async () => {
     stores these in a list of objects which is returned in the allTasks variable
     */
 
-    // const getAllTasksEndpoint = `${baseBackendAPIURL}/tasks/all/`
-
     const imeiCheckTaskEndpoint = `${baseBackendAPIURL}/tasks/imei/`;
     const icloudUnlockTaskEndpoint = `${baseBackendAPIURL}/tasks/icloud/`;
     const carrierUnlockTaskEndpoint = `${baseBackendAPIURL}/tasks/carrier/`;
 
-    let allTasks = [];
-    let apiRequests = [];
+    let allTasks = [], apiRequests = [];
 
     // loop over each task endpoint
-    [imeiCheckTaskEndpoint, icloudUnlockTaskEndpoint, carrierUnlockTaskEndpoint].forEach(endpoint => {
-        // perform get request to retrieve all tasks for each task type
-        const prom = fetch(endpoint).then(response => response.json());
-        apiRequests.push(prom);
+    [imeiCheckTaskEndpoint, icloudUnlockTaskEndpoint, carrierUnlockTaskEndpoint].forEach(taskEndpoint => {
+        // assign the Fetch API definition to a variable
+        // ! The Fetch API is a Promise and so returns a Promise too. 
+        const requestPromises = fetch(taskEndpoint).then(response => response.json());
+        // push the Fetch API to the `apiRequests` array
+        apiRequests.push(requestPromises);
     })
 
+    /*
+    ? return a Promise of a function that finally resolves into an Array of all the task objects `allTasks`
+    ? The Promise concurrently makes the api requests via which the tasks are retrieved as arrays of tasks of the same type
+    */
     return new Promise(resolve => {
-        Promise.all(apiRequests) // make the api requests and returns an array of results of the three api requests
+        /* 
+            This Promise.all concurrently makes the api requests to retrieve the tasks using the Fetch Promises that are in the `apiRequests` array
+            and returns an array of the results (which are also arrays of task objects returned from each request) from the three api requests.
+            Promise.all basically calls the Promises present in the array that is passed as an argument.
+            In this case it makes requests to task endpoints which differ by type of task.
+            Each of these endpoints returns an array of the tasks of a particular type.
+            The returned arrays from each Fetch API request are also stored in an array to which Promise.all resolves - `taskTypes`
+            The array of arrays is accessed by passing it as an argument to the `then` function of the Promise.all() Promise.
+            This array of arrays is looped over and the individual arrays are also looped to retrieve individual tasks.
+            Now the individual tasks are pushed to the `allTasks` array which this `getAllTasks` function shall resolve to.
+            Finally, the magic that makes the difference is another `then` which is called of the previous one. This one takes no arguments and
+            resolves the Promise that is returned by the `getAllTasks` async function into the `allTasks` array. 
+            Therefore the `allTasks` array where all the individual tasks are put asynchronously, 
+            is returned in a promise by the `getAllTasks` function.
+            To access it, a variable to hold it must be passed as an argument to a `then` function called of `getAllTasks` itself.  
+        */    
+        Promise.all(apiRequests) // ? concurrently make requests to task endpoints stored in the array which is passed in as the argument
+            //? loop through the returned array recursively to retrieve and push all individual tasks to the finally resolved array
             .then(taskTypes => {
                 taskTypes.forEach(taskType => {
                     taskType.forEach(task => allTasks.push(task));
-                }) // push individual tasks to allTasks array.
+                })
             })
-                .then(() => resolve(allTasks)) // return array of all tasks
+            .then(() => resolve(allTasks)) // resolve Promise into array of all tasks
     })
-    
 }
 
 
@@ -289,12 +309,12 @@ const getCarrierUnlockTasks = async () => {
                 alert("error")
             }
         })
-            .then(async data => {
-                // console.log(data);
-                for (a in data){
-                    ta.push(a);
-                }
-                return ta;
-            })
+        .then(async data => {
+            // console.log(data);
+            for (a in data) {
+                ta.push(a);
+            }
+            return ta;
+        })
     console.log(ta.length)
 }
